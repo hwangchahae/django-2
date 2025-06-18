419d1f53c2990911cdf26e570e259f7a94990bff
hwangchahae <wndal12@gmail.com>
2025-06-18 12:04:31 +0900

    commit

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from .models import Post
+from django.contrib.auth.decorators import login_required, user_passes_test
+
+def is_in_group(user):
+    return user.groups.filter(name='admin').exists()
+
+@login_required
+@user_passes_test(is_in_group)
+def admin_view(request): # 관리자 전용 뷰
+    pass
+    return render(request, 'myapp/admin.html')
+
+
 # Create your views here.
-def post_list(request):
+def post_list(request): # 아무나 볼 수 있는 뷰 
     posts = Post.objects.all()
     return render(request, 'myapp/post_list.html', {'posts': posts})
+
